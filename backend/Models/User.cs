39d06b362f8c85ac958bd43fccf2c29f549fcccf
amponsemmichael <amponsem_michael@yahoo.com>
2025-12-26 06:59:06 +0000

    Add user entity model

---
+++ backend/Models/User.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    public string PasswordHash { get; set; } = string.Empty;
+
+    [Required]
+    public string Role { get; set; } = "USER";
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}   
